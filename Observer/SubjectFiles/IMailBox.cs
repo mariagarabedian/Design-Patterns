d94c040dea10b2f5ed93349d28a9ca2c0879bd02
Maria Garabedian <maria.garabedian@learnet.se>
2021-12-03 11:01:08 +0100

    Add project files.

---
+++ Observer/SubjectFiles/IMailBox.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace Design_Patterns_Assignment.Observer.SubjectFiles
+{
+    public interface IMailBox : ISubject
+    {
+        public void CreateTimer();
+        public void HandleTimerElapsed(object sender, ElapsedEventArgs e);
+
+    }
+}
